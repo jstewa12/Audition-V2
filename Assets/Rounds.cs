64ffe64f4b9117db115f3b82e774856f2a79959b
Polidori-112 <lukepolidori@gmail.com>
2021-10-23 18:03:35 -0400

    questions

--- Assets/Rounds.cs
+++ Assets/Rounds.cs
@@ -43,15 +43,15 @@ public class Rounds : MonoBehaviour
 
 //array of potential questions: order matters
 //see line 123 if you add questions
-string[] q = {
-"How do you do in front of the camera?",
-"What is your approach to acting in general?",
-"How do you work with other actors, as well as crew and directors?",
-"How do you feel about being included in promotional materials?",
-"How would you describe your work ethic?",
-"Do you know how to sing?",
-"How many languages can you speak?  Your character may need to speak non-English languages.",
-"How athletic/healthy is your lifestyle?  We want our actors as physically fit as possible.",
+string[] question = {
+"How do you do in front of the camera?", 
+"What is your approach to acting in general?", 
+"How do you work with other actors, as well as crew and directors?", 
+"How do you feel about being included in promotional materials?", 
+"How would you describe your work ethic?", 
+"Do you know how to sing?", 
+"How many languages can you speak?  Your character may need to speak non-English languages.", 
+"How athletic/healthy is your lifestyle?  We want our actors as physically fit as possible.", 
 "You may need to dance for your role.  How experienced are you at dancing?",
 "What is your favorite part of acting?",
 "How long have you been acting for?",
@@ -60,17 +60,20 @@ string[] q = {
 "Depending on how much budget we have, we might get a cameo from a big-name actor, like Merly Streep or Dwayne the Rock Johnson. How do you act around big names?",
 "Do you feel frustrated that you are not a famous star?",
 "How do you research and approach a new role?",
-"Your character may have a foreign accent.  How experienced are you in imitating foreign accents?"
+"Your character may have a foreign accent.  How experienced are you in imitating foreign accents?",
+"What do you think of the acting community?",
+"Obviously, acting for various roles involves a lot of flexibility.  No two characters are exactly alike.  How do you handle this?"
 };
+//test
 //array of correct answers: order matters
-string[] yes = {"I do takes consistently well, but am able to take director’s notes!",
-"I try to get into the headspace of my character, through mental, emotional, and physical methods",
+string[] yes = {"I do takes consistently well, but am able to take director’s notes!", 
+"I try to get into the headspace of my character, through mental, emotional, and physical methods", 
 "I can be pretty shy between takes, but I treat coworkers with respect and get my job done well",
 "Anything goes, no matter how I’m portrayed!",
-"I work hard and always show up to work on time.  Whenever I am on set, I am giving 100% effort.  I only call off work when there’s an emergency.",
-"No, I cannot sing.  I am eager to learn how and am willing to spend my free time practicing.",
-"I am fluent in English and can speak a little bit of German.",
-"Every morning, I manage to find time to go for a good run.",
+"I work hard and always show up to work on time.  Whenever I am on set, I am giving 100% effort.  I only call off work when there’s an emergency.", 
+"No, I cannot sing.  I am eager to learn how and am willing to spend my free time practicing.", 
+"I am fluent in English and can speak a little bit of German.", 
+"Every morning, I manage to find time to go for a good run.", 
 "I have been dancing since I was 8 years old.  I took a break for several years during university, but now I have been practicing again.",
 "I really enjoy the freedom that comes with acting.  I can be whomever I want to be, as long as there’s a role for it.  The escape from reality has fascinated me… ever since I was a young child.",
 "I hope to continuously improve my acting career.  Over the next 5 years, I want to gain as much acting experience and knowledge as possible.  While I may not become the most famous actor, I want to have a very successful career in this field.",
@@ -80,28 +83,34 @@ string[] yes = {"I do takes consistently well, but am able to take director’s 
 "I try to act as professionally as possible, on and off camera, even if my mind is going into overdrive!",
 "I am not famous… yet!  I hope to have a successful career and we’ll see where that takes me.",
 "If this character was a real person, I typically do some online research about their history.  If the character is fictitious, I read the script more than once, attempting to internalize their personality.  After that, I take a walk to clear my head and put myself in the right mental state for acting.",
-"Accents are something that I have to work very hard at.  After years of work with my accent coach, I have perfected 2 different English accents and 5 different non-English accents."
+"Accents are something that I have to work very hard at.  After years of work with my accent coach, I have perfected 2 different English accents and 5 different non-English accents.",
+"I love the acting community!  The people here are the absolute nicest people I have ever met.  Most of my friends are into acting, and it helps us form strong connections with each other.  This community is one of the reasons I enjoy acting so much in the first place.",
+"I am great at adapting to new roles.  As long as I get into the right mental state, I find that no matter the character I am playing, it is easy to replicate their behavior."
 };
+
 //array of incorrect answers: order matters
 string[] no = {
-"I get shy in front of cameras and often miss my marks for staging",
-"I often go off script or improvise based on what I feel I would do in the character’s situation.",
+"I get shy in front of cameras and often miss my marks for staging", 
+"I often go off script or improvise based on what I feel I would do in the character’s situation.", 
 "I’m very outgoing and sociable, but shoots take longer as its harder to get me refocused after we cut",
-"I won’t do it unless I discuss with my lawyer!",
-"I work hard!  Typically, I’m only a few minutes late to work.  I may require more breaks than my co-workers, but I do put in effort during the work day.",
-"Yes, I can sing.  I am a sub-par singer and have little passion for music.",
-"I am fluent in English and can fluently understand Spanish and Chinese.",
-"I exercise for 30 minutes every day.  I run around the neighborhood, do push ups, and lift weights.  After every workout, I consume a fast food meal fit for three people.  Who knows why I keep gaining weight…",
-"I really love dancing, and I have been practicing for the last year.  I am a rather casual dancer and my skills are far from professional level.",
+"I won’t do it unless I discuss with my lawyer!", 
+"I work hard!  Typically, I’m only a few minutes late to work.  I may require more breaks than my co-workers, but I do put in effort during the work day.", 
+"Yes, I can sing.  I am a sub-par singer and have little passion for music.", 
+"I am fluent in English and can fluently understand Spanish and Chinese.", 
+"I exercise for 30 minutes every day.  I run around the neighborhood, do push ups, and lift weights.  After every workout, I consume a fast food meal fit for three people.  Who knows why I keep gaining weight…", 
+"I really love dancing, and I have been practicing for the last year.  I am a rather casual dancer and my skills are far from professional level."
 "Acting is the only thing that I have ever been good at.  How else will I pay the bills?",
 "I have been acting all of my life.  It is something that I enjoy, and have been enjoying ever since I could remember.  However, I also have been doing other activities like animation and music production on the side.  I like acting, but in 5 years I may move on with my career.",
-"Yes, going to new places sounds great!  Is this like a vacation?  Will I be able to work less after I’ve travelled there?",
+"Yes, going to new places sounds great!  Is this like a vacation?  Will I be able to work less after I’ve travelled there?"
 "I live 3 miles away, but I have to walk over/need to be picked up in order to arrive on set.",
 "I drool at the mouth and hound them for an autograph. I mean, c’mon, they’re stars!",
 "Yeah, it can be frustrating.  Everyone hopes to make it big and I’m no exception.  Man what I would do for that…",
 "I jump right into the role.  Researching takes time, and that time is better spent with practice in my opinion.  I may not be able to perfect the role, but I’m a bit quicker when we get started without preparation.",
-"Foreign accents are something that come naturally to me.  I can speak in an Australian accent quite easily, but I’ve never tried to imitate anything else."
+"Foreign accents are something that come naturally to me.  I can speak in an Australian accent quite easily, but I’ve never tried to imitate anything else.",
+"I get along very well with the community.  I very rarely make friends with other people who like acting, but I can work with them in a professional setting.",
+"I’m very used to playing the main villain.  I’ve done it so much that I’m extremely good at it.  I can switch between villain roles with ease.  However, I feel out of my element when I try to play any other role."
 };
+
 
 
 
